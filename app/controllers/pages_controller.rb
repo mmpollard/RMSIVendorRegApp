9a6d0abf626d6a10ba36ce0dea43b9f82e961327
Max Pollard <mmpollard@berkeley.edu>
2016-02-22 02:18:36 +0000

    added dummy home page and page controller for heroku placeholder

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    @greeting = 'Welcome to the RMSI Registration page'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  root 'pages#home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
