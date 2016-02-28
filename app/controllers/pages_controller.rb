09b3fd0bf2ea4f8bad3d255da95ff67454ffaad5
Max Pollard <mmpollard@berkeley.edu>
2016-02-28 20:58:25 +0000

    changing splash page to README

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def home
     @greeting = 'Welcome to the RMSI Registration page'
   end
+  
+  def readme
+    @greeting = 'Welcome to the RMSI Registration page'
 end
