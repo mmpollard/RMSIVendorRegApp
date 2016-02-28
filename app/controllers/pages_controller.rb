7a35af6d6a366094bf6f1e4f98c44d11ebc3aa99
Max Pollard <mmpollard@berkeley.edu>
2016-02-28 21:04:55 +0000

    temp fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,4 @@ class PagesController < ApplicationController
   def home
     @greeting = 'Welcome to the RMSI Registration page'
   end
-  
-  def readme
-    @greeting = 'Welcome to the RMSI Registration page'
-  end
 end
