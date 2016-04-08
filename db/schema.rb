1eedc46e6dd37e306163b765df72af2532b8f242
willyoungs <willyoungs13@gmail.com>
2016-04-08 00:53:00 +0000

    Added cuc tests for food vendor

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222012818) do
+ActiveRecord::Schema.define(version: 20160405215151) do
 
 end
