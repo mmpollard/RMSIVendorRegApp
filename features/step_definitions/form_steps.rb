b2c99117c2864c8db862d2bf4019cc73b5aa272e
JustinLaoshi <jcai1@berkeley.edu>
2016-04-13 19:41:52 -0700

    added step defs for cuke to work

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -11,6 +11,42 @@ When /I should see the following (.*): (.*)/ do |inputtype, fieldlist|
     end
   end
 end
+
+When /^(?:|I )select "([^"]*)" for equipment rentals from "([^"]*)"$/ do |value, field|
+  @tempForm = Form.new
+  if field == "6' Table ($10)"
+    whereFrom = :numTables
+    select(value, :from => whereFrom)
+    @tempForm.numTables = value
+  elsif field == "Folding Chair ($5)"
+    whereFrom = :numChairs
+    select(value, :from => whereFrom)
+    @tempForm.numChairs = value
+  elsif field == "11' Market Umbrella ($50)"
+    whereFrom = :numbrellas
+    select(value, :from => whereFrom)
+    @tempForm.numbrellas = value
+  elsif field == "10x10' Food Service Tent ($110)"
+    whereFrom = :numTents
+    select(value, :from => whereFrom)
+    @tempForm.numTents = value
+  end
+end
+
+Then /^the "([^"]*)" field(?: within (.*))? for equipment rentals should contain "([^"]*)"$/ do |field, parent, value|
+  with_scope(parent) do
+      if field == "6' Table ($10)"
+        @tempForm.numTables.should == value
+      elsif field == "Folding Chair ($5)"
+        @tempForm.numChairs.should == value
+      elsif field == "11' Market Umbrella ($50)"
+        @tempForm.numbrellas.should == value
+      elsif field == "10x10' Food Service Tent ($110)"
+        @tempForm.numTents.should == value
+      end
+  end
+end
+
 Then /^the ([^"]*) of "([^"]*)" should be "([^"]*)"$/ do |arg1, arg2, arg3|
    #Implement ME
 end
