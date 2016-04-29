24eb00c3d821df4b48393ff978ada118d48e2ceb
JustinLaoshi <jcai1@berkeley.edu>
2016-04-28 21:59:35 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   
   get '/nonprofit' => 'forms#nonprofit'
   
+  get '/admin' => 'forms#admin'
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
